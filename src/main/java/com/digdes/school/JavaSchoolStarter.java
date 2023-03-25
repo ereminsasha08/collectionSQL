d17bfbdd5f8834f0008aad4fb520a9484d426fe4
ereminsasha08 <SashaEremin08051998@gmail.com>
2023-03-25 12:16:50 +0500

    added exception when uncorrected attribute

--- src/main/java/com/digdes/school/JavaSchoolStarter.java
+++ src/main/java/com/digdes/school/JavaSchoolStarter.java
@@ -4,15 +4,16 @@ import com.digdes.school.model.Request;
 import com.digdes.school.service.ExecuteRequest;
 import com.digdes.school.service.ParserRequest;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 @Getter
+@NoArgsConstructor
 public class JavaSchoolStarter {
     private ParserRequest parserRequest = new ParserRequest();
-    private List<Map<String, Object>> maps;
-    public JavaSchoolStarter() {
-    }
+    private List<Map<String, Object>> maps = new ArrayList<>();
     public JavaSchoolStarter(List<Map<String, Object>> maps) {
         this.maps = maps;
     }
